31cdd2286ec57d811b9a55735d21901c10c945c0
cyruu <133951860+cyruu@users.noreply.github.com>
2024-03-12 19:04:02 +0545

    Delete chatApp directory

--- chatApp/README.md
+++
@@ -1,1 +0,0 @@
-![Capture](https://github.com/cyruu/reactProjects/assets/133951860/9c31a812-ec4a-4592-9365-9783250d03f5)
--- chatApp/src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import chatReducer from "./slices";
-export const store = configureStore({
-  reducer: chatReducer,
-});
